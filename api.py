1855bcd345a3f2cf3723cab2e93fda8119d8d726
Gamey001 <gamaliel.dashua@thepalladiumgroup.com>
2025-02-10 12:25:16 +0100

    added github link to the response data

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI()
 
 @app.get("/")
 def index():
     current_datetime = datetime.now()
-    return {"email": "gdashua@gmail.com","current_datetime":current_datetime,"github_url": ""}
+    return {"email": "gdashua@gmail.com","current_datetime":current_datetime,"github_url": "https://github.com/Gamey001/basic-info-python-api"}
 
